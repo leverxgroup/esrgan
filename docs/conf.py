63404fd2d1951c63a3a3315da443b00668d5f65d
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2021-08-01 12:25:38 +0300

    upd docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "press"
 html_favicon = "_static/index.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
